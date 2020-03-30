906c739f27023e48fe88ea90d4e1a8a1e702fe96
Kaushik <kaushik.makwana@truebil.com>
2020-03-31 01:40:49 +0530

    Updates

---
+++ precache-manifest.b8146a5b3891c0471ff3d8364a5ea831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300984b82f10a80a10938adc343ac295",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "f4d70789c5e7828ade8e",
+    "url": "/dashboard/static/css/main.2c99e390.chunk.css"
+  },
+  {
+    "revision": "4f2d46dc82b456ce4e0e",
+    "url": "/dashboard/static/js/2.a7fe5412.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dashboard/static/js/2.a7fe5412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d70789c5e7828ade8e",
+    "url": "/dashboard/static/js/main.dc150956.chunk.js"
+  },
+  {
+    "revision": "9a34a5c47afc48018561",
+    "url": "/dashboard/static/js/runtime-main.5fa48c14.js"
+  }
+]);
